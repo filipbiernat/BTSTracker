570c7efa4e9d7b60701f6315b2429fa460d01060
filipbiernat <fbiernat@gmail.com>
2017-09-03 20:36:55 +0200

    show arraylist

---
+++ app/src/main/java/pl/edu/agh/student/fbierna/btstracker/ListFragment.java
@@ -0,0 +1,88 @@
+package pl.edu.agh.student.fbierna.btstracker;
+
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+
+
+
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        Log.d("LOGFILIP", "h1");
+        FloatingActionButton fab = (FloatingActionButton) getView().findViewById(R.id.fab_home);
+        Log.d("LOGFILIP", "h2");
+        fab.setOnClickListener(new FloatingActionButtonOnClickListener(fab));
+        Log.d("LOGFILIP", "h3");
+
+    }
+
+    public class FloatingActionButtonOnClickListener implements View.OnClickListener {
+
+        private FloatingActionButton button;
+        private boolean serviceEnabled;
+
+        public FloatingActionButtonOnClickListener(FloatingActionButton button) {
+            this.button = button;
+            serviceEnabled = true;
+            switchState();
+        }
+
+        @Override
+        public void onClick(View arg0) {
+            switchState();
+        }
+
+        private void switchState(){
+            if (serviceEnabled){
+                stopService();
+                Log.d("LOGFILIP", "stopService");
+                //button.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_menu_send));
+            } else {
+                startService();
+                Log.d("LOGFILIP", "startService");
+                //button.setImageDrawable(ContextCompat.getDrawable(getContext(), R.drawable.ic_menu_camera));
+            }
+            //button.setRippleColor(Color.WHITE);
+            serviceEnabled = !serviceEnabled;
+        }
+    }
+
+    public void startService() {
+        Intent intent = new Intent(getActivity(), ScanService.class);
+        getActivity().startService(intent);
+    }
+
+    public void stopService() {
+        Intent intent = new Intent(getActivity(), ScanService.class);
+        getActivity().stopService(intent);
+    }
+}
---
+++ app/src/main/java/pl/edu/agh/student/fbierna/btstracker/list/ListAdapter.java
@@ -0,0 +1,8 @@
+package pl.edu.agh.student.fbierna.btstracker.list;
+
+/**
+ * Created by Filip on 03.09.2017.
+ */
+
+public class PieAdapter {
+}
